c225f5c9f22c89399b1a0ac8285805dcd06a6536
RedTeapot <redmaster.mna@gmail.com>
2016-08-20 17:44:58 +0700

    Create map page

--- src/routes.php
+++ src/routes.php
@@ -4,3 +4,9 @@
 $app->get('/[index.php]', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.html', []);
 });
+
+$app->get('/map[.php]', function($request, $response, $args) {
+    return $this->renderer->render($response, 'map.html', [
+        'map_args' => $request->getUri()->getQuery(),
+    ]);
+});
