cc895d6e96c81fa050cdfe42bad9d522136a0789
RedTeapot <redmaster.mna@gmail.com>
2016-08-20 17:32:22 +0700

    Create index page routing and template

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,6 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    // $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', ['name' => $args['name']]);
+$app->get('/', function ($request, $response, $args) {
+    return $this->renderer->render($response, 'index.html', []);
 });
