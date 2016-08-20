aa91a51845f20c57a0c43508fbcadbba01448fb5
RedTeapot <redmaster.mna@gmail.com>
2016-08-21 00:23:04 +0700

    Add page range validation to player online stats controller

--- src/routes.php
+++ src/routes.php
@@ -42,14 +42,20 @@ $app->get('/newb_info[.php]', function($request, $response, $args) {
 $app->get('/stats[.php]', function($request, $response, $args) {
     global $players_online;
 
+    $stats = new OnlineStats($this->db);
+    $totalCount = $stats->getTotalCount();
+
     $params = $request->getQueryParams();
     $page = isset($params['p']) ?
         intval($params['p']) :
         1;
 
-    $stats = new OnlineStats($this->db);
+    if($page > ceil($totalCount / 50))
+        $page = ceil($totalCount / 50);
+    if($page < 1)
+        $page = 1;
+
     $playersPage = $stats->getStats($page);
-    $totalCount = $stats->getTotalCount();
 
     return $this->renderer->render($response, 'stats.html', [
         'players_online' => $players_online,
