bb8219977a1bbc3127794b0cd47fa2bd7b426a53
RedTeapot <redmaster.mna@gmail.com>
2016-08-20 17:12:57 +0700

    Remove BrowserSync script from index.php

--- public/index.php
+++ public/index.php
@@ -21,7 +21,3 @@ require __DIR__ . '/../src/routes.php';
 $app->run();
 
 ?>
-
-<script id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.14.0.js'><\/script>".replace("HOST", location.hostname));
-//]]></script>
